51b89594654f282d9812a2f7bc0d8f45567bd6be
Olivier Bougeant <olivier.bougeant@impact.com>
2024-08-08 00:20:57 +0200

    Add shortcuts

---
+++ ml_inspector/__init__.py
@@ -0,0 +1,5 @@
+from .gain_curves import plot_gain_curves
+from .precision_recall_curves import plot_precision_recall_curves
+from .roc_curves import plot_roc_curves
+
+__all__ = ["plot_precision_recall_curves", "plot_roc_curves", "plot_gain_curves"]
