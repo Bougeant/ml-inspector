133cb1ebd67c11d9c94c4325dd01cf80170d1d3e
Olivier Bougeant <olivier.bougeant@impact.com>
2024-09-09 00:12:54 +0200

    Use shortcut

--- tests/test_roc_curves.py
+++ tests/test_roc_curves.py
@@ -4,7 +4,7 @@ import numpy as np
 import pytest
 from plotly import graph_objs as go
 
-from ml_inspector import roc_curves
+from ml_inspector import plot_roc_curves
 
 
 class TestROCCurve:
@@ -12,7 +12,7 @@ class TestROCCurve:
         y, y_prob_1, y_prob_2 = binary_predictions
         y_prob = {"Model 1": y_prob_1, "Model 2": y_prob_2}
         class_names = {0: "Class 0", 1: "Class 1"}
-        fig = roc_curves.plot_roc_curves(y, y_prob, class_names=class_names)
+        fig = plot_roc_curves(y, y_prob, class_names=class_names)
         assert isinstance(fig, go.Figure)
         assert len(fig.data) == 3
         assert fig.data[0]["name"] == "Class 1 (Model 1): AUC=1.00"
@@ -23,7 +23,7 @@ class TestROCCurve:
         y, y_prob_1, y_prob_2 = multiclass_predictions
         y_prob = {"Training": y_prob_1, "Test": y_prob_2}
         class_names = {0: "Class 0", 1: "Class 1", 2: "Class 2", 3: "Class 3"}
-        fig = roc_curves.plot_roc_curves(y, y_prob, class_names=class_names)
+        fig = plot_roc_curves(y, y_prob, class_names=class_names)
         assert isinstance(fig, go.Figure)
         assert len(fig.data) == 11
         assert fig.data[0]["name"] == "Class 0 (Training): AUC=1.00"
@@ -39,7 +39,7 @@ class TestROCCurve:
         y = np.array([0, 0, 0, 0, 0])
         y_prob = {"Model 1": y_prob_1, "Model 2": y_prob_2}
         with pytest.raises(ValueError) as ve:
-            roc_curves.plot_roc_curves(y, y_prob)
+            plot_roc_curves(y, y_prob)
         assert str(ve.value) == (
             "Metrics curves are not defined for less than two classes"
         )
@@ -48,7 +48,7 @@ class TestROCCurve:
         y, y_prob_1, y_prob_2 = binary_predictions
         y_prob = {"Model 1": y_prob_1, "Model 2": y_prob_2}
         class_names = {0: "Class 0", 1: "Class 1"}
-        fig = roc_curves.plot_roc_curves(
+        fig = plot_roc_curves(
             y, y_prob, class_names=class_names, decision_threshold=0.4
         )
         assert isinstance(fig, go.Figure)
@@ -62,7 +62,7 @@ class TestROCCurve:
     def test_plot_roc_curves_with_array(self, binary_predictions):
         y, y_prob_1, _ = binary_predictions
         class_names = {0: "Class 0", 1: "Class 1"}
-        fig = roc_curves.plot_roc_curves(y, y_prob_1, class_names=class_names)
+        fig = plot_roc_curves(y, y_prob_1, class_names=class_names)
         assert isinstance(fig, go.Figure)
         assert len(fig.data) == 2
         assert fig.data[0]["name"] == "Class 1 (Predictions): AUC=1.00"
